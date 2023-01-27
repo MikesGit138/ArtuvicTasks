292c12ffc5c53da24101803a654e2e88abc0cd2c
Michael Leighton <michael138leighton@gmail.com>
2023-01-27 17:29:25 -0500

    initial design impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskCardComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/task-card/task-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-card',
+  templateUrl: './task-card.component.html',
+  styleUrls: ['./task-card.component.scss']
+})
+export class TaskCardComponent implements OnInit {
+
+  constructor() { }
+  // task = {
+  //   time: '',
+  //   taskTitle: '',
+  //   completed: true
+  // }
+  ngOnInit(): void {
+  }
+
+}
