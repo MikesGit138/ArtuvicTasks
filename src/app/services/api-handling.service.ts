ae7b508ee40fb5ee5c10debb9fccee30ac84494c
Michael Leighton <michael138leighton@gmail.com>
2023-04-02 10:49:45 -0500

    post task

--- src/app/services/api-handling.service.ts
+++ src/app/services/api-handling.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Task } from '../model/taskClass';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,15 @@ import { Observable } from 'rxjs';
 export class ApiHandlingService {
   
   private getTasksURI = environment.api_route + "/tasks"
-  constructor(private http: HttpClient) { }
-  
+  private createTaskURI = environment.api_route + "/create-task";
 
-  public getTasks():Observable<any[]>{
-    return this.http.get<any[]>(this.getTasksURI);
+  constructor(private http: HttpClient) { }
+
+  public getTasks():Observable<Task[]>{
+    return this.http.get<Task[]>(this.getTasksURI);
+  }
+
+  public createTask(task: Task){
+    return this.http.post<Task>(this.createTaskURI, task);
   }
 }
